73fd7da9b8a3a768d7c29101f4bdf2a466b7cde0
gajapathirao <gajapathiraoguummadi@gmail.com>
2020-04-26 15:41:23 +0530

    NGRX changes added

--- src/app/auth/store/auth.actions.ts
+++ src/app/auth/store/auth.actions.ts
@@ -1,15 +1,20 @@
 import { Action } from '@ngrx/store';
+import { User } from '../user.model';
 
 
-export const LOGIN = 'LOGIN';
-export const LOGOUT = 'LOGOUT';
+export const LOGIN = '[Auth] Login';
+export const LOGOUT = '[Auth] Logout';
 
 export class Login implements Action {
-    readonly type: string;
+    readonly type = LOGIN;
+    constructor(public payload: User) { }
 
 }
 
 export class Logout implements Action {
-    readonly type: string;
-    constructor(public payload: User){}
+    readonly type = LOGOUT;
+    //constructor(public payload: User) { }
 }
+
+
+export type AuthActions = Login | Logout;
--- src/app/auth/store/auth.reduucer.ts
+++ src/app/auth/store/auth.reduucer.ts
@@ -1,14 +1,29 @@
-import { Action } from '@ngrx/store';
 import { User } from '../user.model';
+import * as AuthActions from './auth.actions';
 
 export interface State {
-     user: User;
+    user: User;
 }
 
 const initialState: State = {
     user: null
-}
+};
 
-export function authReducer(state = initialState, action:Action){
-    return state;
+export function authReducer(state = initialState, action: AuthActions.AuthActions) {
+
+    switch (action.type) {
+        case AuthActions.LOGIN:
+            const user = action.payload;
+            return {
+                ...state,
+                user
+            };
+        case AuthActions.LOGOUT:
+            return {
+                ...state,
+                user: null
+            };
+        default:
+            return state;
+    }
 }
